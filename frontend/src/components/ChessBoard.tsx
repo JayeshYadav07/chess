9deffe2d641a8ec0e012a7d7b554f025aa044fc0
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-11-18 13:09:53 +0530

    frontend chess logic add

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,14 +1,26 @@
-import { Color, PieceSymbol, Square } from "chess.js";
+import { Chess } from "chess.js";
+import { useState } from "react";
 
-function ChessBoard({
-	board,
-}: {
-	board: ({
-		square: Square;
-		type: PieceSymbol;
-		color: Color;
-	} | null)[][];
-}) {
+function ChessBoard() {
+	const [chess, setChess] = useState(new Chess());
+	const [from, setFrom] = useState<null | string>(null);
+
+	const handleClick = (i: number, j: number) => {
+		const squarePos = String.fromCharCode(97 + j) + (8 - i);
+		try {
+			if (from) {
+				chess.move({ from, to: squarePos });
+				setChess(new Chess(chess.fen()));
+				setFrom(null);
+			} else {
+				setFrom(squarePos);
+			}
+		} catch (error: any) {
+			setFrom(null);
+			console.error(error.message);
+		}
+	};
+	const board = chess.board();
 	return (
 		<div>
 			{board.map((row, i) => {
@@ -17,12 +29,15 @@ function ChessBoard({
 						{row.map((square, j) => {
 							return (
 								<div
+									onClick={() => {
+										handleClick(i, j);
+									}}
 									key={j}
 									className={`w-16 h-16 flex items-center justify-center ${
 										(i + j) % 2 !== 0
 											? "bg-green-500"
 											: "bg-white"
-									}`}
+									} `}
 								>
 									{square?.type}
 								</div>
--- frontend/src/screen/Game.tsx
+++ frontend/src/screen/Game.tsx
@@ -1,14 +1,10 @@
-import { Chess } from "chess.js";
-import { useState } from "react";
 import Button from "../components/Button";
 import ChessBoard from "../components/ChessBoard";
 function Game() {
-	const [chess, setChess] = useState(new Chess());
-	const [board, setBoard] = useState(chess.board());
 	return (
 		<div className="p-4 max-w-screen-lg grid mx-auto grid-cols-5 gap-4 pt-10">
 			<div className="col-span-4 flex justify-center">
-				<ChessBoard board={board} />
+				<ChessBoard />
 			</div>
 			<div className="col-span-1 bg-slate-800 text-white flex justify-center p-4 rounded-sm">
 				<div>
